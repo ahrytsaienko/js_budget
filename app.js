5f49c410d689067bc1d709dd5cd50d7440680791
temsan89 <temsan89@gmail.com>
2017-06-12 16:35:15 +0300

    IEFI

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-var budgetController = (function ()) {
-var x = 23;
-var add = function (a) {
-    return x + a;
-}
-return {
-    publicTest: function (b) {
-        console.log(add(b));
-    }
-}
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+                        
 })();
 
 
 
+// UI CONTROLLER
 var UIController = (function () {
     // Some code
 })();
 
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function (bungetCtrl, UICtrl) {
 
-})(); 
+    document.querySelector('.add__btn').addEventListener('click', function() {
+       console.log('Button was clicked'); 
+        
+        // 1. Get the field input data
+        
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the iten to the UI
+        
+        // 4. Calculate the budget
+        
+        //5. Display the budget on the UI
+        
+    });
+    
+    document.addEventListener
+    
+})(budgetController, UIController); 
 
-//ff
