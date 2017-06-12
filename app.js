e3176366bee335e2b4a606aeb6cb1a8b589216a2
temsan89 <temsan89@gmail.com>
2017-06-12 19:53:46 +0300

    Lecture: 73

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var budgetController = (function () {
     //    var allIncomes = [];
     //    var totalExpenses = 0;
 
-    
+
     var data = {
         allItems: {
             exp: [],
@@ -27,6 +27,40 @@ var budgetController = (function () {
         total: {
             exp: 0,
             inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //ID = last ID + 1;
+
+
+            // Create new ID
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            //*** this is strange.
+            // Push it into our data stracture
+            data.allItems[type].push(newItem);
+
+            // Return the new element.
+            return newItem;
+        },
+        testing: function () {
+            console.log(data)
         }
     }
 
@@ -68,7 +102,7 @@ var UIController = (function () {
 
 
 // GLOBAL APP CONTROLLER
-var controller = (function (bungetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
     var setupEventListeners = function () {
         var DOM = UICtrl.getDOMstrings();
@@ -85,13 +119,13 @@ var controller = (function (bungetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function () {
-
+        var input, newItem;
 
         // 1. Get the field input data
-        var input = UICtrl.geInput();
+        input = UICtrl.geInput();
         console.log(input)
         // 2. Add the item to the budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the iten to the UI
 
         // 4. Calculate the budget
@@ -110,5 +144,4 @@ var controller = (function (bungetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init(); //IEFI
-dsdsd
 //test
